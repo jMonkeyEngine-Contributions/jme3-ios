5edfca7f6db50b6e73cd4cd381d5d8aecab9dab3
normen667 <normen667@f8aa77f4-c496-4cae-bd6d-9a4d3d61e81d>
2015-01-16 11:15:56 +0000

    up avian version
    Former-commit-id: 4a25e0ea9f3bff2c6315ac69e31da77cb79246dd

--- src/com/jme3/gde/ios/panel/IosCompositeProvider.java
+++ src/com/jme3/gde/ios/panel/IosCompositeProvider.java
@@ -97,7 +97,7 @@ public class IosCompositeProvider implements ProjectCustomizer.CompositeCategory
 
         private String extensionName = "ios";
         private String extensionVersion = "v0.4";
-        private String avianVersion = "v0.5";
+        private String avianVersion = "v1.1";
         private String[] extensionDependencies = new String[]{"jar", "build-ios"};
         private ProjectExtensionManager manager = new ProjectExtensionManager(extensionName, extensionVersion, extensionDependencies);
         private ProjectExtensionProperties properties;
